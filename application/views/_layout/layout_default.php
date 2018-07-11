a2ae468e960b96ba255936dec758ba1db76fa323
Terry Eom <mymizze@gmail.com>
2018-07-11 21:36:57 +0900

    로그아웃 기능 추가

--- application/views/_layout/layout_default.php
+++ application/views/_layout/layout_default.php
@@ -172,7 +172,7 @@
 
             <!-- logout button -->
             <div id="logout" class="btn-header transparent pull-right">
-                <span> <a href="login.html" title="Sign Out" data-action="userLogout" data-logout-msg="You can improve your security further after logging out by closing this opened browser"><i class="fa fa-sign-out"></i></a> </span>
+                <span> <a href="/logout" title="Sign Out" data-action="userLogout" data-logout-msg="브라우저를 닫기 전에 로그아웃을 하면 보안상 더욱 안전합니다."><i class="fa fa-sign-out"></i></a> </span>
             </div>
             <!-- end logout button -->
 
