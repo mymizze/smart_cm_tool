7a44fa1b72f95928019e484dcf5df0cfe52080cc
Terry Eom <mymizze@gmail.com>
2018-07-18 00:08:18 +0900

    상단 Project 드롭다운 메뉴를 Quick 메뉴로 변경

--- application/views/_inc/header.php
+++ application/views/_inc/header.php
@@ -58,23 +58,39 @@
     <!-- #PROJECTS: projects dropdown -->
     <div class="project-context hidden-xs">
 
-        <span class="label">Projects:</span>
-        <span class="project-selector dropdown-toggle" data-toggle="dropdown">Recent projects <i class="fa fa-angle-down"></i></span>
+        <span class="label">FAVORITE</span>
+        <span class="project-selector dropdown-toggle" data-toggle="dropdown">Quick menu <i class="fa fa-angle-down"></i></span>
 
         <!-- Suggestion: populate this list with fetch and push technique -->
         <ul class="dropdown-menu">
             <li>
-                <a href="javascript:void(0);">Online e-merchant management system - attaching integration with the iOS</a>
+                <a href="javascript:void(0);">충전신청</a>
             </li>
             <li>
-                <a href="javascript:void(0);">Notes on pipeline upgradee</a>
+                <a href="javascript:void(0);">환전신청</a>
             </li>
             <li>
-                <a href="javascript:void(0);">Assesment Report for merchant account</a>
+                <a href="javascript:void(0);">총판환전</a>
             </li>
             <li class="divider"></li>
             <li>
-                <a href="javascript:void(0);"><i class="fa fa-power-off"></i> Clear</a>
+                <a href="javascript:void(0);">회원가입</a>
+            </li>
+            <li>
+                <a href="javascript:void(0);">1:1문의</a>
+            </li>
+            <li class="divider"></li>
+            <li>
+                <a href="javascript:void(0);">자동경기1</a>
+            </li>
+            <li>
+                <a href="javascript:void(0);">고액배팅(스포츠)</a>
+            </li>
+            <li>
+                <a href="javascript:void(0);">고액배팅(미니게임)</a>
+            </li>
+            <li>
+                <a href="javascript:void(0);">배팅알람대상ID</a>
             </li>
         </ul>
         <!-- end dropdown-menu-->
